6947c7fdfcf4135d04042d52bbedae0859273c65
Nathaniel Shirts <nshirts7@gmail.com>
2021-03-09 18:09:39 -0700

    creating a react practice app

--- test.js
+++ test.js
@@ -1,1 +1,20 @@
 // test
+
+function toCamelCase(str) {
+  if (str == '') {
+    return str;
+  }
+  let newStr = str.split('-');
+  let newStr2 = str.split('_');
+  let testStr;
+
+  newStr.length > newStr2.length ? (testStr = newStr) : (testStr = newStr2);
+
+  let answer = testStr.map((val, i) => {
+    if (i > 0) {
+      return val[0].toUpperCase() + val.slice(1);
+    } else return val;
+  });
+
+  return answer.join('');
+}
